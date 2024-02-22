ed754258cf7aa50a468ffae569569182e3ebf715
Fahad87667 <ftkhan61814@gmail.com>
2024-02-23 01:10:48 +0530

    Make it

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from nltk.corpus import stopwords
 from nltk.stem import PorterStemmer
 import string
 nltk.download('punkt')
-
+nltk.download('stopwords')
 
 # Function to transform text
 ps = PorterStemmer()
